7019acb718d7d8b6dc3f31979fb74b24b466aba7
Nisha <nishapsarala@gmail.com>
2021-04-06 20:57:55 +0100

    Adding Vendor folder

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ include_once 'JCity.php';
 class Index{
 	private $jsonUrl="http://api.openweathermap.org/data/2.5/weather?units=metric&appid=e8fdfbc37eac561e23d29d942387b187&q=";
 	private $weatherUrl="http://api.openweathermap.org/data/2.5/weather?units=Imperial&appid=e8fdfbc37eac561e23d29d942387b187&q=";
-	/* Constructor to call JCity */
+ 
+ 	/* Constructor to call JCity */
 	
 	function __construct(JCity $juser){
 		$juser->setUrl($this->jsonUrl);
