fc92462107019140e3d5d480ee3919a8c491a9b5
Nisha P Sarala <63558378+nishapsarala@users.noreply.github.com>
2021-04-06 19:23:33 +0100

    Page Modified

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 declare(strict_types=1);
+include_once 'JCity.php';
 class Index{
-	 
+	private $jsonUrl="http://api.openweathermap.org/data/2.5/weather?units=metric&appid=e8fdfbc37eac561e23d29d942387b187&q=";
+	private $weatherUrl="http://api.openweathermap.org/data/2.5/weather?units=Imperial&appid=e8fdfbc37eac561e23d29d942387b187&q=";
+	/* Constructor to call JCity */
+	
+	function __construct(JCity $juser){
+		$juser->setUrl($this->jsonUrl);
+		$juser->setwUrl($this->weatherUrl);
+		$juser->getCityData(); 
+		$juser->getWindData(); 
+		$juser->fnTemplateRedirect();
+	}
 }
- 
+$index= new Index(new JCity());
+$index->fnTemplateRedirect();
+
 ?>
