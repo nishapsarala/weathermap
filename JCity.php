039ec7be0523659210db7e616913f7d5d4acdb96
Nisha P Sarala <63558378+nishapsarala@users.noreply.github.com>
2021-04-06 18:39:10 +0100

    JCity Class for retrieve data

---
+++ JCity.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Class Name : JCity
+ * Description : This class contains all the methods which are required to fetch the data from
+ * API endpoint and display data on custom page.
+ * Author : Nisha Pushpangadan Sarala.
+ */
+declare(strict_types=1);
+class JCity {
+    private $url;  
+  
+
+    /*
+     * Function name : getCityData
+     * Return Type : Array
+     * Description : It retrieves users details from API / Manage error handling for the external HTTP requests .
+    */
+
+    public function getCityData() { 
+        
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=e8fdfbc37eac561e23d29d942387b187&q=London,GB",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => array(
+            "x-rapidapi-host: community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key: [your rapidapi key]"
+          ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+        if ($err) {
+          echo "cURL Error #:" . $err;
+        } else {
+          echo $response;
+        }
+         
+    } 
+ 
+    
+}
+?>
